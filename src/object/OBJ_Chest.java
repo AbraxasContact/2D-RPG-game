26a6533a6c100c68e4b7157e467001f78b9caa34
Emmanuel Simon <musavili2022@gmail.com>
2025-08-15 18:42:58 +0300

    Added interactions with objects in the game

--- src/object/OBJ_Chest.java
+++ src/object/OBJ_Chest.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class OBJ_Chest extends OBJ_Key {
 
     public OBJ_Chest() {
-        name = "chest";
+        name = "Chest";
         try{
             image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/chest.png")));
         } catch (IOException e) {
--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -8,12 +8,13 @@ public class OBJ_Door extends SuperObject{
 
 
     public OBJ_Door() {
-        name = "door";
+        name = "Door";
         try{
             image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/door.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        collision = true;
     }
 
 }

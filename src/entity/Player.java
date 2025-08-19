6cd6b6f3a742c054c51195ea3166fad3a544793d
Emmanuel Simon <musavili2022@gmail.com>
2025-08-19 21:22:46 +0300

    Added a key pickup message

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -150,7 +150,7 @@ public class Player extends Entity{
 					gp.playSE(1);
 					hasKey++;
 					gp.obj[i] = null;
-					System.out.println(hasKey);
+					gp.ui.showMessage("You got a key!");
 					break;
 				case "Door":
 					if (hasKey > 0) {
